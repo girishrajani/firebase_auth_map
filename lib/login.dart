173ab0cb22e0def56f00c7d19a877d113887f33c
Girish Rajani <girish.r.rajani@gmail.com>
2021-05-03 13:33:45 +0530

    Login Screen UI Done

---
+++ lib/login.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'FlutterFire Auth App',
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: ListView(
+            children: [
+              Image(
+                image: NetworkImage(
+                  'https://res.cloudinary.com/practicaldev/image/fetch/s--XVka1yi5--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/6rtttgr7xm8m4tc63jhn.png',
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.GoogleDark,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.GitHub,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.Microsoft,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.AppleDark,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.LinkedIn,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.Twitter,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.Yahoo,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SignInButton(
+                    Buttons.FacebookNew,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
